a03fd01498306da046962a98dd179548cbf78f72
Kieran Schad <kieran.schad@gmail.com>
2022-01-11 00:23:29 -0700

    Create contact page

--- main.js
+++ main.js
@@ -58,3 +58,29 @@ if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navig
     })
   }
 
+// Map
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.006287, lng: -75.707786},
+        zoom: 15,
+        mapId: '94f4ecf15b833169',
+        disableDefaultUI: true,
+        });
+
+    const svgMarker = {
+        path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+        fillColor: "#B52424",
+        fillOpacity: 1,
+        strokeWeight: 0,
+        rotation: 0,
+        scale: 3,
+        anchor: new google.maps.Point(13, 22),
+        };
+
+    const marker = new google.maps.Marker({
+        position: {lat: 40.006287, lng: -75.707786},
+        icon: svgMarker,
+        map: map,
+        });
+}
